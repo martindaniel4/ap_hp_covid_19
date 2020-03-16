cd82cd29823bde6169020d5e5f2e5feb1509aa2c
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-03-16 14:59:21 +0000

    deployment changes

--- api/api.py
+++ api/api.py
@@ -1,9 +1,16 @@
 import time
-from flask import Flask
+from flask import Flask, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 
-@app.route('/time')
+@app.route('/')
+def index():
+    return redirect(url_for('static', filename='index.html'))
+    # another option:
+    # return send_file('../build/index.html')
+
+
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
