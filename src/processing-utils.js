5fe3147601a126ab1b4788022b9e026ddf58f620
Guillaume Galuz <guillaume.galuz@gmail.com>
2020-03-29 10:21:33 -0700

    Add comments in processing util

--- src/processing-utils.js
+++ src/processing-utils.js
@@ -4,8 +4,10 @@ import moment from 'moment'
 export const processFiles = (files) => {
   const { orbis, grims } = files
 
+  // orbisMappedByIPP = {'08290756': {}, '08028476': {}, ...}
   const orbisMappedByIPP = _.groupBy(orbis.data, patient => patient['IPP'])
 
+  // [ {gims_fields + dob}, {}, {} ]
   const currentCovidPatients = grims.data
     .filter(patient => {
       return patient.ipp !== '' && patient.is_pcr === 'Positif' && patient.dt_fin_visite === ''
@@ -20,8 +22,19 @@ export const processFiles = (files) => {
       }
     })
 
+  // { 'hop-uma': [ Patient<>, Patient<>, Patient<> ], 'hop-uma2': [], ...}
   const mapByUMA = _.groupBy(currentCovidPatients, patient => `${patient['hop']} - ${patient['last_uma']}`)
+
+  // [
+  //   {
+  //     hopUma: 'K BICETRE - UMA X',
+  //     covidPatientsAdultCount: 12,
+  //     covidPatientsChildCount: 45,
+  //   },
+  //   {...}
+  // ]
   const tableData = Object.keys(mapByUMA).reduce((acc, hopUma) => {
+    // {adult: 18, child: 24}
     const covidPatientsInHopUmaByAge = _.countBy(mapByUMA[hopUma], patient => {
       return patient.dob && moment(patient.dob, 'DD/MM/YYYY').add(18, 'year').isBefore(moment()) ? 'adult': 'child'
     })
@@ -36,6 +49,6 @@ export const processFiles = (files) => {
   
   return {
     currentCovidPatientsCount: currentCovidPatients.length,
-    tableData
+    tableData,
   }
 }
