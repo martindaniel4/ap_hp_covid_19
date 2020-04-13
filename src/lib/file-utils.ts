f0225885134bd2d709c65a082d2e3d7bb2fb63b9
Guillaume Galuz <guillaume.galuz@gmail.com>
2020-04-13 08:51:11 -0700

    create checkFileForRequiredFields

--- src/lib/file-utils.ts
+++ src/lib/file-utils.ts
@@ -4,12 +4,14 @@ import { CSV_CONFIG } from './constants'
 export function checkFileForErrors(id: FilesIdType, fields: any) {
   let errors = []
 
+  const headerErrors = checkFileForRequiredFields(id, fields)
+
+  return errors.concat(headerErrors)
+}
+
+export function checkFileForRequiredFields(id: FilesIdType, fields: any) {
   const requiredFields = CSV_CONFIG[id]['requiredFields']
   const missingFields = requiredFields.filter(x => !fields.includes(x))
   const headerErrors = missingFields.map(field => { return { message: `${field} est manquant` } })
-  errors = errors.concat(headerErrors)
-
-  console.log(errors)
-
-  return errors
+  return headerErrors
 }
